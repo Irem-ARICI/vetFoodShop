d5ce3ed48b36fb333933d1880a1466856446880a
Irem-ARICI <aricirem@gmail.com>
2024-11-01 16:16:52 +0300

    ilkDers

---
+++ Services/Catalog/vetFoodShop.Catalog/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace vetFoodShop.Catalog.Entities
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
