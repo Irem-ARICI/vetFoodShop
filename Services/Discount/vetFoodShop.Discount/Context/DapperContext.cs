aae838637d9a6b4bcffefe2c3a7ea96e7e4796cc
iremArici <aricirem@gmail.com>
2024-12-15 17:26:22 +0300

    Discount mikroservice (bolum4)

---
+++ Services/Discount/vetFoodShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using vetFoodShop.Discount.Entites;
+
+namespace vetFoodShop.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-C22ONAA;initial Catalog=vetFoodShopCatalogDb;integrated Security=true;TrustServerCertificate=True");
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
---
+++ Services/Discount/vetFoodShop.Discount/Services/IDiscountService.cs
@@ -0,0 +1,13 @@
+﻿using vetFoodShop.Discount.Dtos;
+
+namespace vetFoodShop.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<List<ResultDiscountCouponDto>> GetAllDiscountCouponAsync();
+        Task CreateDiscountCouponAsync(CreateDiscountCouponDto createCouponDto);
+        Task UpdateDiscountCouponAsync(UpdateDiscountCouponDto updateCouponDto);
+        Task DeleteDiscountCouponAsync(int id);
+        Task<GetByIdDiscountCouponDto> GetByIdDiscountCouponAsync(int id);
+    }
+}
