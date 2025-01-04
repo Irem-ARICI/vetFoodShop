27a4d7dbeac716ba2a741b37b12cde27cdef0797
iremArici <aricirem@gmail.com>
2025-01-04 16:28:19 +0300

    angular denemesi öncesi KAYBETME

---
+++ Frontends/vetFoodShop.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace vetFoodShop.WebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
